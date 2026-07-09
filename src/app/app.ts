1b32cbb246aed08c8f974544d19d59b57a783e2f
PassantGamal <basantgamal070@gmail.com>
2026-07-10 02:11:19 +0300

    Complete Home landing page, Navbar, Footer, and color palette (Furqan design)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.css',
---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Carousel } from 'bootstrap';
+import { AfterViewInit, Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements AfterViewInit {
+  courses = [
+    { imagePath:'images/card3.jfif',title: 'الدورة الأولى', level: 'مبتدئ', rating: '★★★★★' },
+    { imagePath:'images/card3.jfif',title: 'الدورة الثانية', level: 'متوسط', rating: '★★★★' },
+    { imagePath:'images/card3.jfif',title: 'الدورة الثالثة', level: 'متقدم', rating: '★★★★★' },
+    { imagePath:'images/card3.jfif',title: 'الدورة الرابعة', level: 'مبتدئ', rating: '★★★★' },
+    { imagePath:'images/card3.jfif',title: 'الدورة الخامسة', level: 'متوسط', rating: '★★★★★' },
+    { imagePath:'images/card3.jfif',title: 'الدورة السادسة', level: 'متقدم', rating: '★★★★' }
+  ];
+
+  ngAfterViewInit(): void {
+    const carouselElement = document.querySelector('#heroCarousel');
+
+    if (carouselElement) {
+      const carousel = new Carousel(carouselElement, {
+        interval: 2500,
+        pause: false,
+        wrap: true
+      });
+
+      setTimeout(() => {
+        carousel.cycle();
+      }, 0);
+    }
+  }
+}
