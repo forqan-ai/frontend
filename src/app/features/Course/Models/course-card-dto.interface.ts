f9018d088fc8ed1c2126d09e373332a0b47b03b1
Ahmed Abdo <ma913222@gmail.com>
2026-08-14 21:54:41 +0300

    show content of lesson to review in admin dashboard

--- src/app/features/Course/Models/course-card-dto.interface.ts
+++ src/app/features/Course/Models/course-card-dto.interface.ts
@@ -1,23 +1,23 @@
 export const CourseLevelArabic: Record<string, string> = {
-    Beginner: 'مبتدئ',
-    Intermediate: 'متوسط',
-    Advanced: 'متقدم',
+  Beginner: 'مبتدئ',
+  Intermediate: 'متوسط',
+  Advanced: 'متقدم',
 };
 
 export interface ICourseCardDto {
-    courseID: string;
-    title: string;
-    description: string,
-    thumbnailURL: string,
-    level: string,
-    categoryID: string,
-    categoryName: string;
-    teacherID: string,
-    teacherName: string,
-    teacherImgUrl: string,
-    durationSeconds: number,
-    rating: number,
-    reviewsCount: number;
-    price: number;
-    createdAt: string;
+  courseID: string;
+  title: string;
+  description: string;
+  thumbnailURL: string;
+  level: string;
+  categoryID: string;
+  categoryName: string;
+  teacherID: string;
+  teacherName: string;
+  teacherImgUrl: string;
+  durationSeconds: number;
+  rating: number;
+  reviewsCount: number;
+  price: number;
+  createdAt: string;
 }
