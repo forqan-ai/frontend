2d9ba66c4de7d6e27b2b8d0e4087c19365b84c02
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-08-23 16:18:55 +0300

    deploy: add Netlify config, production environment, fix gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://forqan-api.runasp.net',
+  auth: {
+    googleClientId: '523741505845-oudn1ji6ljid5ki0s8nas6k57a5baimr.apps.googleusercontent.com',
+    googleLoginUrl: 'https://forqan-api.runasp.net/api/auth/external/google',
+    facebookLoginUrl: 'https://forqan-api.runasp.net/api/auth/external/facebook',
+  },
+};
